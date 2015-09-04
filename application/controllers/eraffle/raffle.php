fbaf0853c4909c83daff074be4743c4d50b52d21
John Doe <johndoe@example.com>
2015-09-04 14:00:31 +0800

    Added Raffle Draw Settings

--- application/controllers/eraffle/raffle.php
+++ application/controllers/eraffle/raffle.php
@@ -4,17 +4,40 @@ class Raffle extends CI_Controller {
         parent::__construct();
         $this->load->helper('eraffle/raffle_helper');           
     }
+    public function set_raffle(){
+        $data = $this->syter->spawn('codes');
+        $data['code'] = setRafflePage();
+        $data['page_title'] = fa('fa-cog')." Set Raffe Draw";
+        $data['load_js'] = 'eraffle/raffle';
+        $data['use_js'] = 'setRaffleJS';
+        // $data['page_no_padding'] = true;
+        // $data['sideBarHide'] = true;
+        $data['add_css'] = array('css/datepicker/datepicker.css','css/daterangepicker/daterangepicker-bs3.css');
+        $data['add_js'] = array('js/plugins/datepicker/bootstrap-datepicker.js','js/plugins/daterangepicker/daterangepicker.js');
+        $this->load->view('page',$data);
+    }    
     public function index(){
         $data = $this->syter->spawn('raffle');
         // $codes = $this->get_valid_codes(false);
+        $range = $_GET['range'];
+        $delay = $_GET['delay'];
         $chars = $this->raffle_chars();
-        $data['code'] = rafflePage($chars);
+        $data['code'] = rafflePage($range,$delay);
         $data['add_js'] = array('js/jquery.shuffleLetters.js','js/pulse.js');
         $data['load_js'] = 'eraffle/raffle';
         $data['use_js'] = 'raffleJs';
         $this->load->view('raffle',$data);
     }    
     public function get_valid_codes($asJson=true){
+        $dates = $this->input->post('range');
+        // $dates = '2015/08/01 12:00 AM to 2015/09/04 12:00 AM';
+        if($dates != ""){
+            $range = explode(' to ', $dates);
+            $from = date2SqlDateTime($range[0]);
+            $to = date2SqlDateTime($range[1]);
+            $args['codes.datetime >= '] = $from;
+            $args['codes.datetime <= '] = $to;
+        }
         $args['codes.email is not null'] = array('use'=>'where','val'=>null,'third'=>false);
         $codes = array();
         $result = $this->site_model->get_tbl('codes',$args);
--- application/helpers/eraffle/raffle_helper.php
+++ application/helpers/eraffle/raffle_helper.php
@@ -1,5 +1,33 @@
 <?php
-function rafflePage($post=array()){
+function setRafflePage($post=array()){
+	$CI =& get_instance();
+	$CI->make->sTab();
+        $tabs = array(
+            fa('fa-cog')." Settings" => array('href'=>'#settings','class'=>'tab_link','load'=>'item/details_load','id'=>'details_link'),
+        );
+        $CI->make->tabHead($tabs,null,array());
+        $CI->make->sTabBody();
+            $CI->make->sTabPane(array('id'=>'settings','class'=>'tab-pane active'));
+                // $CI->make->sForm("settings/db",array('id'=>'email_form'));
+                    $CI->make->sDivRow();
+                        $CI->make->sDivCol(4);
+                        	$CI->make->input('Code Redeem Date & Time Range','calendar_range',null,null,array('class'=>'rOkay daterangepicker datetimepicker','style'=>'position:initial;'),fa('fa-calendar'));
+                        	$CI->make->input('Raffle Delay','delay',150);
+                        $CI->make->eDivCol();
+                    $CI->make->eDivRow();
+                    $CI->make->H(4,"",array('class'=>'page-header'));
+                    $CI->make->sDivRow();
+                	    $CI->make->sDivCol(4,'left',4);
+                	        $CI->make->button(fa('fa-check')." Start Raffle Draw",array('id'=>'start-raffle-btn','class'=>'btn-block'),'success');
+                	    $CI->make->eDivCol();
+                	$CI->make->eDivRow();
+                // $CI->make->eForm();
+            $CI->make->eTabPane();
+        $CI->make->eTabBody();
+    $CI->make->eTab();
+	return $CI->make->code();
+}
+function rafflePage($range,$delay){
 	$CI =& get_instance();
 		$CI->make->sDivRow();
 			$CI->make->sDivCol(4);
@@ -13,7 +41,7 @@ function rafflePage($post=array()){
 							$CI->make->eDiv();
 						$CI->make->eDivCol();
 			    	$CI->make->eDivRow();
-			    	$CI->make->button(fa('fa-circle-thin').' DRAW!!!',array('class'=>'btn-block','id'=>'draw'),'primary');
+			    	$CI->make->button(fa('fa-circle-thin').' DRAW!!!',array('class'=>'btn-block','id'=>'draw','range'=>$range,'delay'=>$delay),'success');
 				$CI->make->eDiv();
 			$CI->make->eDivCol();
 		$CI->make->eDivRow();
